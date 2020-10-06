f269e6accc81128ca4d061047e3c8e3cf0625bed
jonch <9093549+jon-chuang@users.noreply.github.com>
2020-10-06 21:10:43 +0800

    make helpers a same-file module

--- algebra-core/gpu-standalone/examples/main.rs
+++ algebra-core/gpu-standalone/examples/main.rs
@@ -1,7 +1,5 @@
 #![allow(unused)]
 use accel::*;
-mod helpers;
-use crate::helpers::create_pseudo_uniform_random_elems;
 use algebra::bw6_761::G1Projective;
 use algebra_core::{
     curves::ProjectiveCurve, fields::PrimeField, BatchGroupArithmeticSlice, UniformRand,
@@ -21,6 +19,8 @@ const CUDA_GROUP_SIZE: usize = 1 << 5;
 
 pub type G1 = G1Projective;
 pub type BigInt = <<G1Projective as ProjectiveCurve>::ScalarField as PrimeField>::BigInt;
+
+use crate::helpers::create_pseudo_uniform_random_elems;
 
 #[cfg(feature = "cuda")]
 fn main() -> error::Result<()> {
@@ -126,3 +126,38 @@ fn main() -> error::Result<()> {
 
 #[cfg(not(feature = "cuda"))]
 fn main() {}
+
+mod helpers {
+    use algebra_core::{
+        cfg_chunks_mut, AffineCurve, BatchGroupArithmeticSlice, BigInteger64, ProjectiveCurve,
+        UniformRand,
+    };
+    use rand::{distributions::Uniform, prelude::Distribution, Rng};
+
+    #[cfg(feature = "parallel")]
+    use rayon::prelude::*;
+
+    pub fn create_pseudo_uniform_random_elems<C: AffineCurve, R: Rng>(
+        rng: &mut R,
+        max_logn: usize,
+    ) -> Vec<C> {
+        const AFFINE_BATCH_SIZE: usize = 4096;
+        println!("Starting");
+        let now = std::time::Instant::now();
+        // Generate pseudorandom group elements
+        let step = Uniform::new(0, 1 << (max_logn + 5));
+        let elem = C::Projective::rand(rng).into_affine();
+        let mut random_elems = vec![elem; 1 << max_logn];
+        let mut scalars: Vec<BigInteger64> = (0..1 << max_logn)
+            .map(|_| BigInteger64::from(step.sample(rng)))
+            .collect();
+        cfg_chunks_mut!(random_elems, AFFINE_BATCH_SIZE)
+            .zip(cfg_chunks_mut!(scalars, AFFINE_BATCH_SIZE))
+            .for_each(|(e, s)| {
+                e[..].batch_scalar_mul_in_place::<BigInteger64>(&mut s[..], 1);
+            });
+
+        println!("Initial generation: {:?}", now.elapsed().as_micros());
+        random_elems
+    }
+}
