f3f9c27d683964e31383327a7418c9f8eb438841
daosvik <58537287+daosvik@users.noreply.github.com>
2020-12-29 15:38:21 +0100

    feat(asm): Add ARMv8 support, use dedicated squaring functions in x86 (#45)
    * Add ARMv8 assembler implementations for 768-bit modular addition, subtraction and multiplication
    
    * Add ARMv8 assembler implementation of 768-bit modular squaring
    
    * fmt
    
    * fix compilation
    
    * Add x86-64 assembler implementation of 768-bit modular squaring
    
    * Improve layout consistency of 768-bit modular squaring in x86-64 assembler
    
    * fmt
    
    * enable one of x86 and arm assembly
    
    * Rename asm files to follow the pattern <function>-<architecture>.S, and add top-level <function>.S files selecting appropriate arch-specific asm.
    
    * feat(asm): use top level file and preprocess files for each architecture
    
    * fix compilation to use asm on aarch64
    
    * fmt
    
    Co-authored-by: Dag Arne Osvik <osvikd@gmail.com>
    Co-authored-by: Kobi Gurkan <kobigurk@gmail.com>

--- algebra-core/build.rs
+++ algebra-core/build.rs
@@ -1,11 +1,35 @@
 #[cfg(feature = "llvm_asm")]
 use {
     field_assembly::generate_macro_string,
-    std::{env, fs, path::Path},
+    std::{env, fs},
 };
+
+use std::path::{Path, PathBuf};
 
 #[cfg(feature = "llvm_asm")]
 const NUM_LIMBS: usize = 8;
+
+fn replace_extension(file_name: &str, extension: &str) -> String {
+    let mut new_name = PathBuf::from(Path::new(file_name));
+    new_name.set_extension(extension);
+    new_name.as_os_str().to_str().unwrap().to_string()
+}
+
+fn preprocess_and_replace_semicolon(top_level_file: &str, file_names: &[&str], tag: &str) {
+    const EXPANDED_SUFFIX: &str = "SCE.S";
+    const WITHOUT_SEMICOLON_SUFFIX: &str = "SCR.S";
+    for file_name in file_names {
+        let expanded = cc::Build::new().file(file_name).expand();
+        let expanded_path = replace_extension(file_name, EXPANDED_SUFFIX);
+        std::fs::write(&expanded_path, expanded).expect("Should have expanded");
+        let replaced = std::fs::read_to_string(expanded_path)
+            .expect("Should have read expanded file")
+            .replace(";", "\n");
+        let replaced_path = replace_extension(file_name, WITHOUT_SEMICOLON_SUFFIX);
+        std::fs::write(&replaced_path, replaced).expect("Should have written replaced file");
+    }
+    cc::Build::new().file(top_level_file).compile(tag);
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -34,22 +58,50 @@ fn main() {
     let should_use_bw6_asm = cfg!(any(
         all(
             feature = "bw6_asm",
-            target_feature = "bmi2",
-            target_feature = "adx",
-            target_arch = "x86_64"
+            any(
+                all(
+                    target_arch = "x86_64",
+                    target_feature = "bmi2",
+                    target_feature = "adx"
+                ),
+                target_arch = "aarch64"
+            )
         ),
         feature = "force_bw6_asm"
     ));
     if should_use_bw6_asm {
-        cc::Build::new()
-            .file("bw6-assembly/modmul768-sos1-adx.S")
-            .compile("modmul768");
-        cc::Build::new()
-            .file("bw6-assembly/modadd768.S")
-            .compile("modadd768");
-        cc::Build::new()
-            .file("bw6-assembly/modsub768.S")
-            .compile("modsub768");
+        preprocess_and_replace_semicolon(
+            "bw6-assembly/modmul768.S",
+            &vec![
+                "bw6-assembly/modmul768-x86_64.S",
+                "bw6-assembly/modmul768-armv8.S",
+            ],
+            "modmul768",
+        );
+        preprocess_and_replace_semicolon(
+            "bw6-assembly/modadd768.S",
+            &vec![
+                "bw6-assembly/modadd768-x86_64.S",
+                "bw6-assembly/modadd768-armv8.S",
+            ],
+            "modadd768",
+        );
+        preprocess_and_replace_semicolon(
+            "bw6-assembly/modsub768.S",
+            &vec![
+                "bw6-assembly/modsub768-x86_64.S",
+                "bw6-assembly/modsub768-armv8.S",
+            ],
+            "modsub768",
+        );
+        preprocess_and_replace_semicolon(
+            "bw6-assembly/modsqr768.S",
+            &vec![
+                "bw6-assembly/modsqr768-x86_64.S",
+                "bw6-assembly/modsqr768-armv8.S",
+            ],
+            "modsqr768",
+        );
         println!("cargo:rustc-cfg=use_bw6_asm");
     }
 }
